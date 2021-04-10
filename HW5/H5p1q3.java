ed1b995e5bd8fbcace00cac488b3d1c1143330fb
Glacier-Han <hbh6449@naver.com>
2021-04-11 02:34:34 +0900

    h5p1q3

---
+++ HW5/H5p1q3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class H5p1q3{
+	public static void main(String[] args){
+		String type;
+		int line;
+		Scanner sc = new Scanner(System.in);
+		while(true){
+			System.out.print("라인 수: ");
+			type = sc.nextLine();
+			if(type.equals("피라미드")||type.equals("사각형")||type.equals("다이아몬드")) break;
+			System.out.println("[입력오류]");
+		}
+		
+		if(type.equals("피라미드")){
+			
+		}
+		
+		else if(type.equals("사각형")){
+			
+		}
+		
+		else {
+			
+		}
+		
+		
+	}
+}
+
